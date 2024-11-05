b0b2854b77c6222525ec7176064287a214f53d73
Vigneshwaran G <146413175+Vigneshwaran-Ganesan@users.noreply.github.com>
2024-11-05 21:58:31 +0530

    Update LibraryApp.java

--- LibraryApp.java
+++ LibraryApp.java
@@ -1,38 +1,48 @@
 package libraryApp;
 
 public class LibraryApp {
-	
-	BookRepository repo = new BookRepository();
-	
-	public void findByTitle(String title) {
-		repo.searchByTitle(title);
-		return;
-	}
-	public void findByISBN(int isbn) {
-		repo.searchByISBN(isbn);
-		return;
-	}
-	public boolean findByGenre(String genre) {
-		if(repo.searchByGenre(genre))
-			return true;
-		else 
-			return false;
-	}
-	
-	
-	public int findISBN(int isbn) {
-		return repo.searchISBN(isbn);
-	}
-	
-	public boolean withdrawBook(int isbn) {
-		return repo.getBook(isbn);
-	}
-	
-	public boolean depositBook(int isbn) {
-		return repo.submitBook(isbn);
-	}
 
-	public void getStatus(int isbn) {
-		repo.bookStatus(isbn);
-	}
+    BookRepository repo = new BookRepository();
+
+    public void findByTitle(String title) {
+        if (repo.searchByTitle(title)) {
+            System.out.println("Books found with title containing: " + title);
+        } else {
+            System.out.println("No books found with title containing: " + title);
+        }
+    }
+
+    public void findByISBN(int isbn) {
+        if (repo.searchByISBN(isbn)) {
+            System.out.println("Book found with ISBN: " + isbn);
+        } else {
+            System.out.println("No book found with ISBN: " + isbn);
+        }
+    }
+
+    public boolean findByGenre(String genre) {
+        boolean found = repo.searchByGenre(genre);
+        if (found) {
+            System.out.println("Books found in genre: " + genre);
+        } else {
+            System.out.println("No books found in genre: " + genre);
+        }
+        return found;
+    }
+
+    public int findISBN(int isbn) {
+        return repo.searchISBN(isbn);
+    }
+
+    public boolean withdrawBook(int isbn) {
+        return repo.getBook(isbn);
+    }
+
+    public boolean depositBook(int isbn) {
+        return repo.submitBook(isbn);
+    }
+
+    public void getStatus(int isbn) {
+        repo.bookStatus(isbn);
+    }
 }
